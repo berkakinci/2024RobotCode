e507215f82e3f36e26849ffc0df9b7d7efd014c3
FRCTEAM1474 <frcteam1474@gmail.com>
2024-01-17 17:54:43 -0500

    limelightlib stuff, will be removed. committed for posterity

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,9 +1,36 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.LimelightHelpers;
-import edu.wpi.first.apriltag.*;
-import edu.wpi.first.networktables.*;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class LimelightSubsystem {
-    
+public class LimelightSubsystem extends SubsystemBase {
+    public double tx = 0;
+    public double ty = 0;
+    public double ta = 0;
+    public double apriltagID;
+    public double pipelineID;
+
+    public LimelightSubsystem(){
+        
+    }
+
+    @Override
+    public void periodic(){
+        tx = LimelightHelpers.getTX("limelight");
+        ty = LimelightHelpers.getTY("limelight");
+        ta = LimelightHelpers.getTA("limelight");
+        apriltagID = LimelightHelpers.getFiducialID("limelight");
+        pipelineID = LimelightHelpers.getCurrentPipelineIndex("limelight");
+
+
+    }
+
+    public static double shootingAngle(double apriltagID, double tx, double ty, double ta) {
+        double angle = 0;
+
+        angle = ()
+
+        return angle;
+    }
+
+
 }
