12d616772101e63b2598fcfe913ac6e290ae2aca
FRCTEAM1474 <frcteam1474@gmail.com>
2024-02-07 19:28:17 -0500

    fixed angle offsets, module position offsets, and changed pid

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,9 +69,9 @@ public class RobotContainer
       // Applies deadbands and inverts controls because joysticks
       // are back-right positive while robot
       // controls are front-left positive
-      () -> MathUtil.applyDeadband(driverXbox.getLeftY(),
+      () -> MathUtil.applyDeadband(-driverXbox.getLeftY(),
         OperatorConstants.LEFT_Y_DEADBAND),
-      () -> MathUtil.applyDeadband(driverXbox.getLeftX(),
+      () -> MathUtil.applyDeadband(-driverXbox.getLeftX(),
         OperatorConstants.LEFT_X_DEADBAND),
       () -> -driverXbox.getRightX(),
       () -> -driverXbox.getRightY());
